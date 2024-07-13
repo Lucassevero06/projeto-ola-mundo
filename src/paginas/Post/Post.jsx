bf013d06593afc60966756181738d67ae779b4e1
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-13 15:32:49 -0400

    react é bglh doido mermo

--- src/paginas/Post/Post.jsx
+++ src/paginas/Post/Post.jsx
@@ -1,18 +1,28 @@
 import { useParams } from "react-router-dom";
-import { posts } from "../../json/posts.json";
+import posts from "../../json/posts.json";
+import PostModelo from "../../componentes/PostModelo/PostModelo";
+import ReactMarkdown from "react-markdown";
+import './Post.css';
 
 const Post = () => {
 
     const parametros = useParams();
 
     const post = posts.find((post) => {
-        return post.id === parametros.id;
+        return post.id === Number(parametros.id);
     });
 
     return (
-        <div>
-            {parametros.id}
-        </div>
+        <PostModelo 
+            fotoCapa={`../../../public/assets/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+        </PostModelo>
     )
 }
 export default Post
